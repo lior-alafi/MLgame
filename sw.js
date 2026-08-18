5d687d18228be2b469f525b840348ca40b24c558
lior-alafi <104000856+lior-alafi@users.noreply.github.com>
2026-08-18 22:10:03 +0300

    Cache v13 math-memory assets

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE = 'ml-recall-v12-20260818';
+const CACHE = 'ml-recall-v13-20260818';
 const CORE = [
-  './', './index.html', './styles.css?v=12', './app.js?v=12', './question-bank.js?v=12',
-  './manifest.webmanifest?v=12', './math-mode.js?v=12', './icons/icon-192.png', './icons/icon-512.png'
+  './', './index.html', './styles.css?v=13', './app.js?v=13', './question-bank.js?v=13',
+  './course-math-v13.js?v=13', './math-mode.js?v=13', './why-context-v13.js?v=13',
+  './manifest.webmanifest?v=13', './icons/icon-192.png', './icons/icon-512.png'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
